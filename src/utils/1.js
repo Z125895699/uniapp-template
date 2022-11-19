67672be9264f7679483566c71fea10d1c6f2090d
zhangdi <1258956799@qq.com>
2022-11-19 22:54:22 +0800

    init code

---
+++ src/utils/1.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: zhangdi 1258956799@qq.com
+ * @Date: 2022-11-19 22:51:51
+ * @LastEditors: zhangdi 1258956799@qq.com
+ * @LastEditTime: 2022-11-19 22:52:03
+ * @FilePath: /demo/src/utils/1.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+export let list = [];
